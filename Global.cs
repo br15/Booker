60832cd0ae6edb5249c9703d4577d932971a9c79
keith <keith@keithbalaam.com>
2020-06-29 17:09:51 +0100

    Add project files.

---
+++ Global.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using Booker.Properties;
+using Newtonsoft.Json;
+
+namespace Booker
+{
+    public class Global
+    {
+        const string JSON_EXTENSION = ".JSON";
+
+        public Global()
+        {
+
+        }
+
+        #region Public properties.
+        int[] Courts                        { get; set; } = new int[] {1,2,3,4,5,6,7};
+
+        DateTime DateOfGame                 { get; set; } = DateTime.Today;
+
+        TimeSpan TimeOfGame                 { get; set; } = new TimeSpan(9, 0, 0);
+
+        TimeSpan StartRequestingCourtAt     { get; set; } = new TimeSpan(8, 0, 0);
+
+        DateTime StartRequestingCourtOn     { get; set; } = DateTime.Today.AddDays(14);
+
+
+
+        int LengthOfGameInMinutes { get; set; }
+        #endregion
+        #region Serialize/Deserialize methods.
+        public bool Serialize(string fileNamePath)
+        {
+            bool result = true;
+
+            try
+            {
+                using(StreamWriter file = File.CreateText(fileNamePath))
+                {
+                    JsonSerializer serializer = new JsonSerializer();
+                    serializer.Serialize(file, this);
+                }
+            }
+            catch(Exception ex)
+            {
+                result = false;
+            }
+            return result;
+        }
+
+        public static Global Deserialize(string fileNamePath)
+        {
+            Global global = null;
+
+            try
+            {
+                // Make sure it's a JSON file.
+                string ext = Path.GetExtension(Settings.Default.RuntimeSettingFile).ToUpper();
+
+                if(ext == JSON_EXTENSION)
+                {
+                    // Make sure that the file exists.
+                    if(File.Exists(Settings.Default.RuntimeSettingFile))
+                    {
+                        using(StreamReader file = File.OpenText(fileNamePath))
+                        {
+                            JsonSerializer serializer = new JsonSerializer();
+                            global = (Global) serializer.Deserialize(file, typeof(Global));
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return global;
+        }
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using Booker.Properties;
+
+
+namespace Booker
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Global global = null;
+
+            // Validate our parameters.
+            if(args == null || args.Length == 0)
+            {
+                // No parameters passed. See if we have the location set for our default run-time settings file.
+                if(Settings.Default.RuntimeSettingFile != null && Settings.Default.RuntimeSettingFile.Length > 0)
+                {
+                    // Get our run-time settings.
+                    global = Global.Deserialize(Settings.Default.RuntimeSettingFile);
+
+                    // Tell the user that we failed to retrieved our run-time parameters file.
+                    if(global == null)
+                    {
+                        Console.WriteLine($"No parameters were passed and we couldn't load '{Settings.Default.RuntimeSettingFile}'.");
+                    }
+                }
+
+                // See if there's a default runtimeparams.json file in the execution directory. 
+                string fileNamePath = $@"{AppDomain.CurrentDomain.BaseDirectory}runtimeparams.json";
+
+
+                // Try and get our run-time settings from the execution directory.
+                global = Global.Deserialize(fileNamePath);
+
+                // Tell the user that we failed to retrieved our run-time parameters file.
+                if(global == null)
+                {
+                    Console.WriteLine($"No run-time parameters were found in the execution directory '{fileNamePath}'.{Environment.NewLine}Bye!");
+
+                    return 1;
+                }
+            }
+
+            // Override any settings in our run-time parameter
+            new Args(args, global);
+
+            return 0;
+        }
+    }
+}
